fb0cd44408e614ee673d994fbc9516eb21fd36f7
Christopher Nelson <Cnelson10@users.noreply.github.com>
2018-05-16 11:14:51 -0400

    Add files via upload

---
+++ Maze.java
@@ -0,0 +1,27 @@
+//Christopher Nelson
+//27 January 2018
+//Lab05: Mazes and Stacks
+package maze;
+
+import java.awt.GridLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Maze extends JFrame {
+
+    public Maze() {
+
+        this.add(new MazeGridPanel(100, 100));
+        this.setSize(800, 800);
+
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.pack();
+        this.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Maze();
+    }
+
+}
